900327bb847662eab6f90ef047a91cf448c9acbe
Yulissa22 <ec1413220@gmail.com>
2024-08-08 08:21:40 -0600

    commit 1

---
+++ src/main/java/org/esfe/Test01JAVAEEEYCD/Test01JavaeeeycdApplication.java
@@ -0,0 +1,13 @@
+package org.esfe.Test01JAVAEEEYCD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01JavaeeeycdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test01JavaeeeycdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfe/Test01JAVAEEEYCD/Test01JavaeeeycdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.Test01JAVAEEEYCD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test01JavaeeeycdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
